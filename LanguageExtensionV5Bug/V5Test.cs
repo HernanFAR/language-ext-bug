bcc0dbe22c518ea031642c9238e44d8d362c8b11
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2024-07-29 00:41:43 -0400

    Added gitignore

--- LanguageExtensionV5Bug/V5Test.cs
+++ LanguageExtensionV5Bug/V5Test.cs
@@ -1,34 +1,20 @@
-using LanguageExt;
 using LanguageExt.Common;
-using static LanguageExt.Prelude;
 
 namespace LanguageExtensionV5Bug;
 
 public class V5Test
 {
-    public sealed record CustomExpected(string Message, int Code, string Another)
-        : Expected(Message, Code);
+    public sealed record NotFound(string Message) : Expected(Message, 404);
 
     [Fact]
     public void Test()
     {
         // Arrange
-        var expected = new CustomExpected("Name", 100, "This is loss");
-        Eff<int> effect = liftEff<int>(() => expected);
+        Error expected = new NotFound("Name");
 
         // Act
-        Fin<int> fin = effect.Run();
+        var act = $"Expected: {expected}";
 
-        // Assert
-        fin.Match(_ => Assert.True(false),
-                  error =>
-                  {
-                      Assert.Equal(error.Code, expected.Code);
-                      Assert.Equal(error.Message, expected.Message);
-
-                      var fail = (CustomExpected)error;
-                      Assert.Equal(fail.Another, expected.Another);
-                  });
-
+        Assert.NotNull(act);
     }
 }
